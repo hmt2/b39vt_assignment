89196a5084b3c0dd610a9392a9ea03417fdf8029
hmt2 <hmt2@hw.ac.uk>
2017-02-13 11:33:01 +0000

    first commit

--- src/image_processing.cpp
+++ src/image_processing.cpp
@@ -1,6 +1,96 @@
 #include "b39vt_assignment/image_processing.hpp"
 
-void templateMatching(const cv::Mat& im, const cv::Mat& templ)
+#include "b39vt_assignment/image_processing.hpp"
+
+#include <ros/ros.h>
+#include <image_transport/image_transport.h>
+#include <cv_bridge/cv_bridge.h>
+#include <sensor_msgs/image_encodings.h>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <string>
+
+#include "geometry_msgs/Point.h"
+#include "std_msgs/String.h"
+
+
+  void templateMatching(const cv::Mat& img, const cv::Mat tem, const std::string sign)
 {
-	// Your code goes here
+        const char * image_window = "/usb_cam/image_raw";
+        const char * template_window = "Template window";
+        
+
+    //const std::vector< std::pair<cv::Mat, std::string> >& templ
+        
+        
+        
+    
+    //Create confidence threshold
+        double threshold = 1.5;
+        /// Source image to display
+        cv::Mat img_display, result;
+        int match_method = CV_TM_SQDIFF;
+        img.copyTo( img_display );
+
+        /// Create the result matrix
+        int result_cols =  img.cols - tem.cols + 1;
+        int result_rows = img.rows - tem.rows + 1;
+        //ROS_INFO("Matrix creation...");
+        result.create( result_rows, result_cols, CV_32FC1 );
+
+        //ROS_INFO("Matrix created...");
+        /// Do the Matching and Normalize
+        cv::matchTemplate( img, tem, result, match_method );
+
+        //cv::normalize( result, result, 0, 1, cv::NORM_MINMAX, -1, cv::Mat() );
+        
+
+        /// Localizing the best match with minMaxLoc
+        double minVal; double maxVal; cv::Point minLoc; cv::Point maxLoc;
+        cv::Point matchLoc;
+
+        cv::minMaxLoc( result, &minVal, &maxVal, &minLoc, &maxLoc, cv::Mat() );
+        /// For SQDIFF and SQDIFF_NORMED, the best matches are lower values. For all the other methods, the higher the better
+        //ROS_INFO(("%d"), minVal) ;
+
+     double newVal = minVal/1000000000;
+
+        //printf("newVal %f\n",newVal);
+        if(newVal < threshold)
+        {
+            if( match_method  == CV_TM_SQDIFF || match_method == CV_TM_SQDIFF_NORMED )
+             { 
+                matchLoc = minLoc;
+             }
+             
+            else
+              { 
+                 matchLoc = maxLoc; 
+              }
+    
+
+
+            cv::rectangle( img_display, matchLoc, cv::Point( matchLoc.x + tem.cols , matchLoc.y + tem.rows) , cv::Scalar::all(0), 2, 8, 0 );
+
+            cv::rectangle( result, matchLoc, cv::Point( matchLoc.x + tem.cols , matchLoc.y + tem.rows ), cv::Scalar::all(0), 2, 8, 0 );
+
+
+            cv::imshow( image_window, img );
+                    
+                   
+            
+            printf("-----------------------------------------------------%d,%d\n", matchLoc.x, matchLoc.y);
+            
+            //imshow( result_window, result );
+        }
+     
+            else{cv::imshow( image_window, img );}
+            //ROS_INFO("No match");}
+    //  return matchLoc;
+        
+            // Update GUI Window
+    cv::imshow(template_window, tem);
+        //cv::imshow(image_window, img);
+    cv::waitKey(3);
 }
+
--- src/image_subscriber.cpp
+++ src/image_subscriber.cpp
@@ -4,33 +4,44 @@
 #include <sensor_msgs/image_encodings.h>
 #include <opencv2/imgproc/imgproc.hpp>
 #include <opencv2/highgui/highgui.hpp>
+#include "geometry_msgs/Point.h"
+#include "std_msgs/String.h"
+
+
+#include <string>
 
 #include "b39vt_assignment/image_processing.hpp"
 
 class ImageSubscriber
 {
-	// By default, variables and methods are private
+    // By default, variables and methods are private
   ros::NodeHandle nh_;
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
-  
-  cv_bridge::CvImagePtr cv_ptr;
-  
+  image_transport::Publisher image_pub_;
+    //const char * image_window = "/usb_cam/image_raw";
+//  const char * template_window = "Template window";
+
   const std::string OPENCV_WINDOW;
   
 public:
-	// Public variables - it is a good practice to separate the declaration of
-	// variables from the declaration/definition of methods
-	bool data_valid;
+    // Public variables - it is a good practice to separate the declaration of
+    // variables from the declaration/definition of methods
+      
+  cv_bridge::CvImagePtr cv_ptr;
+  
+    bool data_valid;
   
 public:
-	// Public methods
+    // Public methods
   ImageSubscriber() : it_(nh_), data_valid(false), OPENCV_WINDOW("Image window")
   {
     // Subscribe to input video feed
     image_sub_ = it_.subscribe("/usb_cam/image_raw", 1, 
       &ImageSubscriber::imageCb, this);
+  //  image_pub_ = it_.advertise("/image_converter/output_video", 1);
 
+   
     cv::namedWindow(OPENCV_WINDOW);
   }
 
@@ -39,6 +50,13 @@ public:
     cv::destroyWindow(OPENCV_WINDOW);
   }
 
+
+  cv::Mat getImage(){ //to access cv_ptr in main method
+  
+  return cv_ptr->image;
+  
+  }
+  
   void imageCb(const sensor_msgs::ImageConstPtr& msg)
   {
     try
@@ -64,17 +82,73 @@ int main(int argc, char** argv)
   ros::init(argc, argv, "image_subscriber");
   ImageSubscriber ic;
   
-  cv::Mat a;
-  cv::Mat b;
+  //cv::Mat a;
+  //cv::Mat b;
+  
+        
+    std::vector< std::pair<cv::Mat, std::string> > templ;
+            
+          templ.push_back(std::pair<cv::Mat, std::string>(cv::imread("/home/calum/Pictures/Test_Images/biohazard.png"),"biohazard"));
+          templ.push_back(std::pair<cv::Mat, std::string>(cv::imread("/home/calum/Pictures/Test_Images/danger.png"),"danger"));
+          templ.push_back(std::pair<cv::Mat, std::string>(cv::imread("/home/calum/Pictures/Test_Images/fire.png"),"fire"));
+          templ.push_back(std::pair<cv::Mat, std::string>(cv::imread("/home/calum/Pictures/Test_Images/greeen_helmet.png"),"green_helmet"));
+          templ.push_back(std::pair<cv::Mat, std::string>(cv::imread("/home/calum/Pictures/Test_Images/red_helmet.png"),"red_helmet"));
+          templ.push_back(std::pair<cv::Mat, std::string>(cv::imread("/home/calum/Pictures/Test_Images/radioactive.png"),"radioactive"));
+          templ.push_back(std::pair<cv::Mat, std::string>(cv::imread("/home/calum/Pictures/Test_Images/smoking.png"),"smoking"));
+          templ.push_back(std::pair<cv::Mat, std::string>(cv::imread("/home/calum/Pictures/Test_Images/toxic.png"),"toxic"));
+          
+          
+    
+    /*tempMap[cv::imread("/home/calum/Pictures/Test_Images/biohazard.png")] = "biohazard";
+    tempMap[cv::imread("/home/calum/Pictures/Test_Images/danger.png")] = "danger";
+    tempMap[cv::imread("/home/calum/Pictures/Test_Images/fire.png")] = "fire";
+    tempMap[cv::imread("/home/calum/Pictures/Test_Images/green_helmet.png")] = "green_helmet";
+    tempMap[cv::imread("/home/calum/Pictures/Test_Images/red_helmet.png")] = "red_helmet";
+    tempMap[cv::imread("/home/calum/Pictures/Test_Images/radioactive.png")] = "radioactive";
+    tempMap[cv::imread("/home/calum/Pictures/Test_Images/smoking.png")] = "smoking";
+    tempMap[cv::imread("/home/calum/Pictures/Test_Images/toxic.png")] = "toxic";
+    
+    
+  //array of templates
+        cv::Mat templ[8]; 
+        
+        templ[0] = cv::imread("/home/calum/Pictures/Test_Images/biohazard.png");
+        templ[1] = cv::imread("/home/calum/Pictures/Test_Images/danger.png");
+        templ[2] = cv::imread("/home/calum/Pictures/Test_Images/fire.png");
+        templ[3] = cv::imread("/home/calum/Pictures/Test_Images/green_helmet.png");
+        templ[4] = cv::imread("/home/calum/Pictures/Test_Images/red_helmet.png");
+        templ[5] = cv::imread("/home/calum/Pictures/Test_Images/radioactive.png");
+        templ[6] = cv::imread("/home/calum/Pictures/Test_Images/smoking.png");
+        templ[7] = cv::imread("/home/calum/Pictures/Test_Images/toxic.png");
+  */
   
   while (ros::ok())
   {
-		if (ic.data_valid)
-		{
-			templateMatching(a, b);
-			// Your code goes here
-		}
-  	ros::spinOnce();
+        if (ic.data_valid)
+        {
+            //templateMatching(cv_ptr->image, templ);
+            for(int i = 0;i < 7; i++) //change condition to .size()
+                {
+                    cv::Mat tem = templ[i].first;
+                            std::string sign = templ[i].second;
+                    
+                    printf("%d,%d",tem.cols, tem.rows);
+                    
+                    cv::Size size(235,235);//template size,e.g.100x100
+                    cv::resize(tem,tem,size);//resize image
+                            
+                            
+                    //cv::imshow(template_window, templ[i]);
+
+                
+                    templateMatching(ic.getImage(), tem, sign);
+                    cv::waitKey(3);
+                }
+    
+ 
+        }
+    ros::spinOnce();
   }
   return 0;
 }
+
