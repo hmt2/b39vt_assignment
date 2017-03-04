cc2b4b8fe384c24fe336694fc4e858ac9f2c1b0f
hmt2 <hmt2@hw.ac.uk>
2017-03-04 15:37:56 +0000

    working object avoidance code

--- src/object_avoidance.cpp
+++ src/object_avoidance.cpp
@@ -6,10 +6,8 @@
 class RobotDriver
 {
 private:
-    //The node handle we'll be using
     ros::NodeHandle nh_;
-    //We will be publishing to the "/base_controller/command" topic to issue commands
-    ros::Publisher cmd_vel_pub_;
+    ros::Publisher cmd_vel_pub;
     ros::Subscriber scanSub;
 
 public:
@@ -23,6 +21,7 @@ public:
         scanSub = nh_.subscribe<sensor_msgs::LaserScan>("/scan",1, &RobotDriver::processLaserScan, this);
     }
 
+    //find average method
     float findAverage(const std::vector<float>& range, int minRange, int maxRange)
     {
         float temp = 0;
@@ -48,30 +47,32 @@ public:
         return temp;
     }
 
-
+    //callback method
     void processLaserScan(const sensor_msgs::LaserScanConstPtr& scan)
     {
         geometry_msgs::Twist base_cmd;
         int inf = 0;
-        float a, b, c, c1, d, e;
-        //float front, left, right;
+        float a, b, c, d, e, f;
 
+        //default movement of robot
         base_cmd.linear.x = 0.1;
         cmd_vel_pub_.publish(base_cmd);
 
+        //call findAverage method for each range segment
         a = findAverage(scan->ranges,0,90); //size 90
         b = findAverage(scan->ranges,90,180); //size 90
         c = findAverage(scan->ranges,180,256); //size 76
-        c1 = findAverage(scan->ranges,256,332); //size 76
-        d = findAverage(scan->ranges,332,422); //size 90
-        e = findAverage(scan->ranges,422,512); //size 90
+        d = findAverage(scan->ranges,256,332); //size 76
+        e = findAverage(scan->ranges,332,422); //size 90
+        f = findAverage(scan->ranges,422,512); //size 90
         printf("a = %f\n", a);
         printf("b = %f\n", b);
         printf("c = %f\n", c);
         printf("d = %f\n", d);
         printf("e = %f\n", e);
+        printf("e = %f\n", f);
 
-        //set cases for switch statement
+        //set case conditions for switch statement for each segment
         int val;
 
         if(a<0.2)
@@ -79,123 +80,102 @@ public:
             val = 1;
         }
 
-        if(b < 0.3)
+        if(b<0.3)
         {
             val = 2;
         }
 
-        if(c < 0.2)
+        if(c<0.2)
+        {
+            val = 3;
+        }
+
+        if(d<0.2)
         {
             val = 4;
         }
 
-        if (c1<0.2)
+        if(e<0.3)
         {
-            val = 44;
+            val = 5;
         }
 
-        if(d < 0.3)
+        if(f<0.2)
         {
-            val =6;
-        }
-
-        if(e < 0.2)
-        {
-            val = 8;
+            val = 6;
         }
 
 
         switch(val)
         {
 
-        case 1 :
+        case 1 : //a<0.2
         {
             base_cmd.linear.x = 0;
             base_cmd.angular.z = 1;
 
             cmd_vel_pub_.publish(base_cmd);
+
         }
             break;
 
-        case 2 :
+        case 2 : //b<0.3
         {
             base_cmd.linear.x = 0;
             base_cmd.angular.z = 2;
+
             cmd_vel_pub_.publish(base_cmd);
+
         }
             break;
 
-     /*   case 3 :
-        {
-            base_cmd.linear.x = 0;
-            base_cmd.angular.z = 1;
-            //  base_cmd.angular.x = -1;
 
-            cmd_vel_pub_.publish(base_cmd);
-        }
-            break;
-*/
-        case 4 :
+        case 3 : //c<0.2
         {
             base_cmd.linear.x = 0;
             base_cmd.angular.z = 1.5;
 
             cmd_vel_pub_.publish(base_cmd);
             sleep(1);
+
         }
             break;
 
-        case 44 :
+        case 4 : //d<0.2
         {
             base_cmd.linear.x = 0;
             base_cmd.angular.z = -1;
 
             cmd_vel_pub_.publish(base_cmd);
             sleep(1);
-        }
-            break;
-  /*      case 5 :
-        {
-            base_cmd.linear.x = 0;
-            base_cmd.angular.z = 1.5;
 
-            cmd_vel_pub_.publish(base_cmd);
-            sleep(1);
         }
             break;
-*/
-        case 6 :
+
+        case 5 : //e<0.3
         {
             base_cmd.linear.x = 0;
             base_cmd.angular.z = -2;
 
             cmd_vel_pub_.publish(base_cmd);
+
         }
             break;
 
- /*       case 7 :
-        {
-            base_cmd.linear.x = 0;
-            base_cmd.angular.z = 1;
-            //  base_cmd.angular.x = -1;
 
-            cmd_vel_pub_.publish(base_cmd);
-        }
-            break;
-*/
-        case 8 :
+        case 6 : //f<0.2
         {
             base_cmd.linear.x = 0;
             base_cmd.angular.z = -1;
 
             cmd_vel_pub_.publish(base_cmd);
+
         }
             break;
 
-        default :
+        default : //if no case conditions met, default to moving forward
             base_cmd.linear.x = 0.1;
         }
-
 
     }
 
